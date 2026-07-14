0ea54aa72fcf6e72d571a0f1cc9832a27a4d428f
b.b.o.y.b.l.a.1.s.e <blaisetuyishime98@gmail.com>
2026-07-14 16:10:08 +0200

    Add startGame function for Snake Game

---
+++ script.js
@@ -0,0 +1,12 @@
+function startGame(){
+    alert("🎮 Snake Game is starting soon!");
+
+    let player = prompt("Enter your gamer name:");
+
+    if(player){
+        alert("Welcome " + player + "! Good luck 🎯");
+    }
+    else{
+        alert("Welcome Gamer! 🎮");
+    }
+}
